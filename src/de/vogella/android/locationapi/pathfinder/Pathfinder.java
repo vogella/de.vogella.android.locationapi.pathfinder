a398ddec922da2caf3cd15ce54512f17215e35ce
Lars Vogel <Lars.Vogel@gmail.com>
2010-06-15 23:23:12 +0200

    Haensel and Gretel Application finished

--- src/de/vogella/android/locationapi/pathfinder/Pathfinder.java
+++ src/de/vogella/android/locationapi/pathfinder/Pathfinder.java
@@ -1,6 +1,7 @@
 package de.vogella.android.locationapi.pathfinder;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,38 +12,51 @@ import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.OverlayItem;
 
 public class Pathfinder extends MapActivity {
 
 	private MapView mapView;
 	private MapController mapController;
+	private MyOverlays itemizedoverlay;
+	
 
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
-		setContentView(R.layout.main);
+		setContentView(R.layout.main); // bind the layout to the activity
 		mapView = (MapView) findViewById(R.id.mapview);
 		mapView.setBuiltInZoomControls(true); // ZoomControls
-		mapView.setSatellite(true); // Set View to Satellite
+		mapView.setSatellite(false); // Set View to Satellite
 		mapController = mapView.getController();
 		mapController.setZoom(14);
 		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
-				0, new GeoUpdateHandler());
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				0, 5, new GeoUpdateHandler());
 		initMyLocation();
+
+		Drawable drawable = this.getResources()
+		.getDrawable(R.drawable.point);
+		itemizedoverlay = new MyOverlays(drawable);
+		
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
 	}
 
 	public class GeoUpdateHandler implements LocationListener {
+
 		@Override
 		public void onLocationChanged(Location location) {
 			int lat = (int) (location.getLatitude() * 1E6);
 			int lng = (int) (location.getLongitude() * 1E6);
 			mapController.animateTo(new GeoPoint(lat, lng));
+			createMarker();
 		}
-		// Leere Methoden weggelassen
 
 		@Override
 		public void onProviderDisabled(String provider) {
-			
 		}
 
 		@Override
@@ -52,6 +66,7 @@ public class Pathfinder extends MapActivity {
 		@Override
 		public void onStatusChanged(String provider, int status, Bundle extras) {
 		}
+
 	}
 
 	private void initMyLocation() {
@@ -60,10 +75,14 @@ public class Pathfinder extends MapActivity {
 		myLocOverlay.enableCompass();
 		mapView.getOverlays().add(myLocOverlay);
 	}
-
-	@Override
-	protected boolean isRouteDisplayed() {
-		return false;
+	
+	private void createMarker() {
+		GeoPoint p = mapView.getMapCenter();
+		OverlayItem overlayitem = new OverlayItem(p, "", "");
+		itemizedoverlay.addOverlay(overlayitem);
+		mapView.getOverlays().add(itemizedoverlay);
 	}
 
+
+	
 }
